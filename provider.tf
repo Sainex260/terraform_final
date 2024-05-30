102f4443796929b3a0744f7ffeeb860452d89617
Sainex260 <67476192+Sainex260@users.noreply.github.com>
2024-05-31 02:59:55 +0530

    Update provider.tf
    added provider aws

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
-      source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "ap-south-1"
-  profile = "default"
+  region     = "ap-south-1"
+  access_key = "AKIAXHG6J7G4QTXT6GZA"
+  secret_key = "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"
 }
-*/
-
