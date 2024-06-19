312274dda8dbbc9b4b7a45cc69880e04cdb05aca
Sainex260 <67476192+Sainex260@users.noreply.github.com>
2024-06-19 16:49:05 +0530

    Update main.tf
    updated main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-09edb498ada567cb6" # Amazon Linux 2 AMI (change as needed)
   instance_type = "t2.medium"
-  subnet_id     = "subnet-e8240b80"
-  vpc_security_group_ids = [aws_security_group.jenkins_sg2.id]
 
   tags = {
     Name = "SSPwebdeployment"
   }
-
-  # Add a key pair (replace with your key name)
-  key_name = "jenkins"
 }
